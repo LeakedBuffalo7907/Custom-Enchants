c4c6d210d7353277619e34350b2697cf549d85b9
LeakedBuffalo7907 <69818555+LeakedBuffalo7907@users.noreply.github.com>
2021-01-01 20:43:02 -0500

    remove old code untill i get a fix

--- src/leakedce/enchants/Lifesteal.php
+++ src/leakedce/enchants/Lifesteal.php
@@ -15,15 +15,4 @@ class Lifesteal implements Listener{
 	public function __construct(){
 		Main::get()->getServer()->getPluginManager()->registerEvents($this, Main::get());
 	}
-
-	public function onDamage(EntityDamageEvent $event): void{
-		if($event instanceof EntityDamageByEntityEvent){
-			$damager = $event->getDamager();
-
-			if(!$damager instanceof Player) return;
-			if($damager->getInventory()->getItemInHand()->getEnchantment(100)){
-				$damager->setHealth($damager->getMaxHealth());
-			}
-		}
-	}
 }
